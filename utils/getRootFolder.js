fd7ddffe1ef706613dd1f537df39efe0e17dbff2
Sascha Goldhofer <post@saschagoldhofer.de>
2020-03-10 17:57:21 +0100

    Fix lint issue

--- utils/getRootFolder.js
+++ utils/getRootFolder.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 
-module.exports = function getRootFolder(sourcePath, entry, partials) {
+module.exports = function getRootFolder(sourcePath, entry) {
     const domen = entry.split("*")[0];
     // array of all path elements except the relative ones (**|*)
     const rootArray = domen.split(path.sep);
