3bde0776c02562fc0d778af49e70730d495a4034
Sascha Goldhofer <post@saschagoldhofer.de>
2020-03-10 17:56:55 +0100

    Remove check where entrypoint is within partialsfolder

--- utils/getRootFolder.js
+++ utils/getRootFolder.js
@@ -7,36 +7,5 @@ module.exports = function getRootFolder(sourcePath, entry, partials) {
     const rootArray = domen.split(path.sep);
     const rootFolderName = rootArray[rootArray.length - 2];
 
-    if (partials) {
-        let isPartialsPath = false;
-
-        // ignore partials paths
-        partials.forEach(partial => {
-            let partialRootIndex = null;
-
-            // find parent folder for relatives (**|*)
-            if (partial.split(path.sep).indexOf("**") !== -1) {
-                partialRootIndex = partial.split(path.sep).indexOf("**") - 1;
-            } else if (partial.split(path.sep).indexOf("*") !== -1) {
-                partialRootIndex = partial.split(path.sep).indexOf("*") - 1;
-            }
-
-            if (partialRootIndex) {
-                // partial folder name
-                const partialFolderName = partial.split(path.sep)[partialRootIndex];
-                // current source folder name
-                const folderName = path.dirname(sourcePath).split(path.sep)[partialRootIndex];
-                // ignore partial
-                if (folderName === partialFolderName) {
-                    isPartialsPath = true;
-                }
-            }
-        });
-
-        if (isPartialsPath) {
-            return false;
-        }
-    }
-
     return rootFolderName;
 };
